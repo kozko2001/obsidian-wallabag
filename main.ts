d397513fe4347b006a151a93905a34f43ebd07e8
Jordi Coscolla <kozko2001@gmail.com>
2022-10-17 02:58:24 +0200

    add jest, and first api to auth

--- main.ts
+++ main.ts
@@ -1,137 +1,198 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { Secret } from './secret';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
+}
+
+type AuthResponseError = {
+  error: string,
+  error_description: string
+}
+
+export type AuthResponseSuccess = {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  token_type: 'bearer';
+};
+
+
+type AuthResponse = AuthResponseError | AuthResponseSuccess
+
+function isResponseError(result: any): result is AuthResponseError {
+  return typeof result === 'object' && typeof result.error === 'string'
+}
+
+class WallabagAPI {
+  constructor(
+    private clientId: string,
+    private clientSecret: string,
+    private username: string,
+    private password: string,
+  ) { }
+  async auth() {
+    const body = {
+      grant_type: 'password',
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      username: this.username,
+      password: this.password,
+    }
+
+    const domain = 'wallabag.coscolla.net';
+    const url = `https://${domain}/oauth/v2/token`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data: AuthResponse = await response.json() as AuthResponse
+
+    if (isResponseError(data)) {
+      console.log("error...", data.error_description)
+    } else {
+      console.log('success!', data)
+    }
+
+    console.log(data)
+  }
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Wallabag plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Wallabag plugin notice');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon('dice', 'Wallabag plugin', (_: MouseEvent) => {
+      // Called when the user clicks the icon.
+      new Notice('Wallabag plugin notice');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('wallabag Status Bar Text');
+    });
+    // Perform additional things with the ribbon
+    ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'wallabag-open-sample-modal-simple',
-			name: 'wallabag simple model',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText('wallabag Status Bar Text');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: 'wallabag-open-sample-modal-simple',
+      name: 'wallabag simple model',
+      callback: () => {
+        const secrets = new Secret()
+        const api = new WallabagAPI(secrets.clientId, secrets.clientSecret, secrets.username, secrets.password)
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        api.auth().then(() => { console.log('done') });
+        new SampleModal(this.app).open();
+      }
+    });
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: 'sample-editor-command',
+      name: 'Sample editor command',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection('Sample Editor Command');
+      }
+    });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: 'open-sample-modal-complex',
+      name: 'Open sample modal (complex)',
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      }
+    });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
 
-	onunload() {
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      console.log('click', evt);
+    });
 
-	}
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  onunload() {
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText('Woah kzk!');
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for wallabag awesome plugin.'});
+    containerEl.createEl('h2', { text: 'Settings for wallabag awesome plugin.' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          console.log('Secret: ' + value);
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
